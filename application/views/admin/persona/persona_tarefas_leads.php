ae2028dcdd70ada2b4f86dde413635d321730541
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-01-19 03:36:26 -0300

    .

--- application/views/admin/persona/persona_tarefas_leads.php
+++ application/views/admin/persona/persona_tarefas_leads.php
@@ -223,7 +223,7 @@
 
                         
                             
-                            <tr class="mt-3 mb-3">
+                            <tr class="mt-5 mb-5">
                             <td><?= $l->id; ?></td>
 
                                 <td><a target="_blank" href="https://instagram.com/<?= $l->username ?>"><?= $l->full_name; ?></a></td>
@@ -265,7 +265,7 @@
                                     <?php } ?> </td>
 
                             </tr>
-                           <br>
+                           
 
                         <?php } ?>
 
