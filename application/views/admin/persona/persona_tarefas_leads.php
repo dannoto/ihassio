dfaf059a1e00d17195b895776e791dab3e8b8913
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-01-19 03:51:54 -0300

    .

--- application/views/admin/persona/persona_tarefas_leads.php
+++ application/views/admin/persona/persona_tarefas_leads.php
@@ -244,7 +244,7 @@
 
                                 <td><?= $l->email; ?></td>
                                 <td><?= $l->telefone; ?></td>
-                                <td > <button onclick="getInstaLeadDemanda(<?= $l->tarefa_id ?>, <?= $l->tag_id ?>, '<?= $l->username ?>')"  data-bs-toggle="modal" data-bs-target="#modalInfo"  class="btn btn-danger" ><small>VER</small></button> </td>
+                                <td class="mb-5" > <button onclick="getInstaLeadDemanda(<?= $l->tarefa_id ?>, <?= $l->tag_id ?>, '<?= $l->username ?>')"  data-bs-toggle="modal" data-bs-target="#modalInfo"  class="btn btn-danger" ><small>VER</small></button> </td>
                                   <?php if ($l->convertido == 0) { ?>
                                     <td > <button onclick="convertInapto(<?= $l->id ?>)"   class="btn btn-light" ><small>INAPTO</small></button> </td>
                                   <?php } else { ?>
