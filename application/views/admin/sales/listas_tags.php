0d87628050b4375d2020e0a88643d083e749e170
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-02-09 21:38:02 -0300

    adicionando funcao de concluir tarefas

--- application/views/admin/sales/listas_tags.php
+++ application/views/admin/sales/listas_tags.php
@@ -162,11 +162,11 @@
                 <button class="btn btn-light text-primary border border-0"> <span class="bx bx-left-arrow-alt"></span> VOLTAR    </button>
                 </a>
               </div>
-              <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light"></span> Listas - <?= $this->admin_model->get_item($c->tag)['nome']; ?></h4>
+              <h4 class="fw-bold py-3 mb-1"><span class="text-muted fw-light"></span> Listas - <?= $this->admin_model->get_item($c->tag)['nome']; ?></h4>
 
               <!-- Basic Bootstrap Table -->
               <a href="<?= base_url() ?>sales/lista_adicionar">
-                  <button class="btn btn-primary mt-2 mb-3"  type="button" >+ ADICIONAR</button>
+                  <button class="btn btn-primary mt-1 mb-3"  type="button" >+ ADICIONAR</button>
               </a>
 
               <div class="card">
