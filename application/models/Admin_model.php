a51d3609fc02fcebd5b65b348bb6f676b1df5e66
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-02-11 01:16:28 -0300

    adicionando funcao de concluir tarefas

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -850,23 +850,36 @@ class Admin_model extends CI_Model
         return $this->db->insert('campanhas', $data);
     }
 
-    public function getLeadsToSynchronizeCampanhaAssociada($lista_id, $campanha_id, $quantidade_max)
-    {
-        $sql = 'SELECT DISTINCT pc.lead_id
-                    FROM campanha_prospection pc
-                    WHERE pc.campanha_id = ' . $campanha_id . '
-                    AND pc.is_deleted = 0
-                    AND pc.lead_id NOT IN (
-                        SELECT li.person_id
-                        FROM leads_import li
-                        WHERE li.lista_id = ' . $lista_id . '
-                    )  LIMIT ' . $quantidade_max . '';
+    // public function getLeadsToSynchronizeCampanhaAssociada($lista_id, $campanha_id, $quantidade_max)
+    // {
+    //     $sql = 'SELECT DISTINCT pc.lead_id
+    //                 FROM campanha_prospection pc
+    //                 WHERE pc.campanha_id = ' . $campanha_id . '
+    //                 AND pc.is_deleted = 0
+    //                 AND pc.lead_id NOT IN (
+    //                     SELECT li.person_id
+    //                     FROM leads_import li
+    //                     WHERE li.lista_id = ' . $lista_id . '
+    //                 )  LIMIT ' . $quantidade_max . '';
     
-        $query = $this->db->query($sql);
+    //     $query = $this->db->query($sql);
     
-        return $query->result();
-    }
-    
+    //     return $query->result();
+    // }
+    public function getLeadsToSynchronizeCampanha($campanha_id, $lista_id, $quantidade_max)
+{
+    $this->db->distinct();
+    $this->db->select('pc.lead_id');
+    $this->db->from('campanha_prospection pc');
+    $this->db->where('pc.campanha_id', $campanha_id);
+    $this->db->where('pc.is_deleted', 0);
+    $this->db->where_not_in('pc.lead_id', "(SELECT li.person_id FROM leads_import li WHERE li.lista_id = $lista_id)");
+    $this->db->limit($quantidade_max);
+    $query = $this->db->get();
+
+    return $query->result();
+}
+
 
     public function getLeadsToSynchronize($lista_id, $tag_id, $quantidade_max)
     {
