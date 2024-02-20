600f5b1562b09b5bee23c7ca9d46503fb086a2cb
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-02-20 02:07:56 -0300

    tungstenio steps

--- index.php
+++ index.php
@@ -102,7 +102,7 @@ switch (ENVIRONMENT)
 		// {
 		// 	error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
 		// }
-	error_reporting(-1);
+	error_reporting(1);
 		ini_set('display_errors', 1);
 	break;
 
