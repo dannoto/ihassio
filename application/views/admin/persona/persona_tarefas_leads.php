8b802350765305442fd69c96f944bee2fb0f6d56
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-02-26 22:14:32 -0300

    .

--- application/views/admin/persona/persona_tarefas_leads.php
+++ application/views/admin/persona/persona_tarefas_leads.php
@@ -186,7 +186,7 @@
                       <div class="d-flex  align-items-center justify-content-between">
                         <div>
                         <H4>Leads - Filtro</H4>
-                        <?php print_r($this->admin_model->coutx_inaptos($t['id']));?>
+                        <p> <small>INAPTOS: <?=count($this->admin_model->coutx_inaptos($t['id']))?> | APTOS: <?=count($this->admin_model->coutx_aptos($t['id']))?> | CONVERTIDOS: <?=count($this->admin_model->coutx_convertidos($t['id']))?>  </small>    </p>
                             <p><?= $t['tarefa_nome'] ?></p>
 
                         </div>
